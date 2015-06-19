ff0991eba0e3c120ce5f5ec23df02a0d80af5225
mechairoi <ttsujikawa@gmail.com>
2015-06-19 17:02:12 +0900

    test

---
+++ src/test/scala/interndiary/application/DiaryAppSpec.scala
@@ -0,0 +1,14 @@
+package interndiary.application
+
+import interndiary.helper._
+import org.joda.time.LocalDateTime
+
+import scala.util.Random
+
+class DiaryAppSpec extends UnitSpec with SetupDB {
+  describe("DiaryApp") {
+    it("should be DiaryApp") {
+      "DiaryApp" shouldBe "DiaryApp"
+    }
+  }
+}
