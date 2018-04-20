2e052230a481776e66708e9d4ed6196a7d83bf14
voidsatisfaction <lourie@naver.com>
2018-04-20 16:33:11 +0900

    feat: add db schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,33 @@
--- CREATE TABLE user (
---    `id` BIGINT UNSIGNED NOT NULL,
---    PRIMARY KEY (id)
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `user_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(32) NOT NULL,
+  `created` TIMESTAMP NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+DROP TABLE IF EXISTS `diary`;
+CREATE TABLE `diary` (
+  `diary_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(150) NOT NULL,
+  `user_id` BIGINT UNSIGNED NOT NULL,
+  `created` TIMESTAMP NOT NULL,
+  PRIMARY KEY (`diary_id`),
+  UNIQUE KEY (`title`, `user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- QUESTION: これないと、errorが出る。どうすれば良い？
+SET SQL_MODE='ALLOW_INVALID_DATES';
+
+DROP TABLE IF EXISTS `article`;
+CREATE TABLE `article` (
+  `article_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(150) NOT NULL,
+  `body` VARCHAR(5000) NOT NULL,
+  `diary_id` BIGINT UNSIGNED NOT NULL,
+  `created` TIMESTAMP NOT NULL,
+  `updated` TIMESTAMP NOT NULL,
+  PRIMARY KEY (`article_id`),
+  UNIQUE KEY (`title`, `diary_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
