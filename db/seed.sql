9fdfefd24bd10da4a3a8d811ad70bcc8fb15c585
voidsatisfaction <lourie@naver.com>
2018-04-26 13:06:08 +0900

    chore: delete auto incremenmt

--- db/seed.sql
+++ db/seed.sql
@@ -1,28 +1,28 @@
-INSERT INTO user (name, created)
+INSERT INTO user (user_id, name, created)
   VALUES
-    ('test1', '2007-01-01'),
-    ('test2', '2007-01-01'),
-    ('test3', '2007-01-01');
+    (1, 'test1', '2007-01-01'),
+    (2, 'test2', '2007-01-01'),
+    (3, 'test3', '2007-01-01');
 
-INSERT INTO diary (user_id, title, created)
+INSERT INTO diary (diary_id, user_id, title, created)
   VALUES
-    (1, 'diary1', '2007-01-01'),
-    (1, 'diary2', '2007-01-01'),
-    (2, 'diary3', '2007-01-01');
+    (1, 1, 'diary1', '2007-01-01'),
+    (2, 1, 'diary2', '2007-01-01'),
+    (3, 2, 'diary3', '2007-01-01');
 
-INSERT INTO article (diary_id, title, body, created, updated)
+INSERT INTO article (article_id, diary_id, title, body, created, updated)
   VALUES
-    (1, 'monster', 'hello monster', '2007-01-01', '2017-01-01'),
-    (1, 'monster2', 'hello monster222', '2008-01-10', '2017-01-01'),
-    (2, 'monster3', 'hello monster3', '2009-01-10', '2017-01-01'),
-    (3, 'monster22312', 'hello monster33', '2010-01-10', '2017-01-01'),
-    (1, 'monster123', 'hello monster222', '2008-01-10', '2017-01-01'),
-    (1, 'monsteasdfar2', 'hello monster222', '2008-01-10', '2017-01-01'),
-    (1, 'mons123ter22', 'hello monster222', '2008-01-10', '2017-01-01'),
-    (1, 'monstwer2', 'hello monster222', '2008-01-10', '2017-01-01'),
-    (1, 'monster22312stwer2', 'hello monster222', '2008-01-10', '2017-01-01'),
-    (1, 'mons1t er2', 'hello monster222', '2008-01-10', '2017-01-01'),
-    (1, 'mons1231123ter2', 'hello monster222', '2008-01-10', '2017-01-01'),
-    (1, 'mons3ter2', 'hello monster222', '2008-01-10', '2017-01-01'),
-    (1, 'mons4ter2', 'hello monster222', '2008-01-10', '2017-01-01'),
-    (1, 'mons5ter2', 'hello monster222', '2008-01-10', '2017-01-01');
+    (1, 1, 'monster', 'hello monster', '2007-01-01', '2017-01-01'),
+    (2, 1, 'monster2', 'hello monster222', '2008-01-10', '2017-01-01'),
+    (3, 2, 'monster3', 'hello monster3', '2009-01-10', '2017-01-01'),
+    (4, 3, 'monster22312', 'hello monster33', '2010-01-10', '2017-01-01'),
+    (5, 1, 'monster123', 'hello monster222', '2008-01-10', '2017-01-01'),
+    (6, 1, 'monsteasdfar2', 'hello monster222', '2008-01-10', '2017-01-01'),
+    (7, 1, 'mons123ter22', 'hello monster222', '2008-01-10', '2017-01-01'),
+    (8, 1, 'monstwer2', 'hello monster222', '2008-01-10', '2017-01-01'),
+    (9, 1, 'monster22312stwer2', 'hello monster222', '2008-01-10', '2017-01-01'),
+    (10, 1, 'mons1t er2', 'hello monster222', '2008-01-10', '2017-01-01'),
+    (11, 1, 'mons1231123ter2', 'hello monster222', '2008-01-10', '2017-01-01'),
+    (12, 1, 'mons3ter2', 'hello monster222', '2008-01-10', '2017-01-01'),
+    (13, 1, 'mons4ter2', 'hello monster222', '2008-01-10', '2017-01-01'),
+    (14, 1, 'mons5ter2', 'hello monster222', '2008-01-10', '2017-01-01');
