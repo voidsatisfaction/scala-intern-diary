edbc2471a9ca580e01519cc5d96aab020c3cf817
voidsatisfaction <lourie@naver.com>
2018-04-27 17:08:14 +0900

    test: add User findByName test

--- src/test/scala/interndiary/repository/Users.scala
+++ src/test/scala/interndiary/repository/Users.scala
@@ -8,14 +8,15 @@ class UserRepositorySpec extends UnitSpec with SetupDB with Factory {
       it("should be created with name") {
         val name = randomString(10)
         val createdUser = Users.create(name)
+        val foundedUser = Users.findByName(createdUser.name)
 
-        createdUser.name shouldBe name
+        createdUser.name shouldBe foundedUser.getOrElse(null).name
       }
     }
 
     describe("findByName") {
       it("should return user") {
-        val dUser = dummyUser()
+        val dUser = dummyUser
 
         val fetchedUser = Users.findByName(dUser.name).getOrElse(null)
 
