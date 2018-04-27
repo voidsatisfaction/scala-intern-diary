d4519924d53c226fc51ed9e80a8de501cee79d98
voidsatisfaction <lourie@naver.com>
2018-04-27 16:08:57 +0900

    feat: add User repository and its tests

---
+++ src/main/scala/interndiary/repository/Users.scala
@@ -0,0 +1,34 @@
+package interndiary.repository
+
+import interndiary.model.User
+import scala.concurrent.ExecutionContext.Implicits.global
+import org.joda.time.LocalDateTime
+import slick.driver.MySQLDriver.api._
+import slick.jdbc.GetResult
+import com.github.tototoshi.slick.MySQLJodaSupport._
+
+object Users {
+  private implicit val getUserRowResult = GetResult(r => User(r.<<, r.<<, r.<<))
+  def create(name: String)(implicit ctx: Context): User = {
+    /* QUESTION: なんで、|　が使えないでしょうか。使ったらエラー */
+    val userId: Long = Identifier.generate
+    val user = User(userId, name, new LocalDateTime())
+    run(sqlu"""
+    INSERT INTO user (user_id, name, created)
+    VALUES (
+      ${user.userId},
+      ${user.name},
+      ${user.created}
+    )
+    """)
+    user
+  }
+
+  def findByName(name: String)(implicit ctx: Context): Option[User] = {
+    run(sql"""
+    SELECT * FROM user
+    WHERE name = $name
+    LIMIT 1
+    """.as[User].map(_.headOption))
+  }
+}
---
+++ src/test/scala/interndiary/repository/Users.scala
@@ -0,0 +1,27 @@
+package interndiary.repository
+
+import interndiary.helper._
+
+class UserRepositorySpec extends UnitSpec with SetupDB with Factory {
+  describe("User Repository") {
+    describe("create") {
+      it("should be created with name") {
+        val name = randomString(10)
+        val createdUser = Users.create(name)
+
+        createdUser.name shouldBe name
+      }
+    }
+
+    describe("findByName") {
+      it("should return user") {
+        val dUser = dummyUser()
+
+        val fetchedUser = Users.findByName(dUser.name).getOrElse(null)
+
+        fetchedUser should not be null
+        fetchedUser.name shouldBe dUser.name
+      }
+    }
+  }
+}
