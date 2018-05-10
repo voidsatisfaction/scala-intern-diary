8e499b5ad511f93e9e2a2a72173ed66c0dc38098
voidsatisfaction <lourie@naver.com>
2018-05-10 17:59:04 +0900

    feat: add service part

--- src/main/scala/interndiary/service/DiaryApp.scala
+++ src/main/scala/interndiary/service/DiaryApp.scala
@@ -1,6 +1,6 @@
 package interndiary.service
 
-import interndiary.model.{User, Diary}
+import interndiary.model.{User, Diary, Article}
 import interndiary.repository
 
 class DiaryApp(currentUserName: String) {
@@ -8,12 +8,45 @@ class DiaryApp(currentUserName: String) {
     repository.Users.findOrCreateByName(currentUserName)
   }
 
-  def addDiary(title: String)(implicit ctx: Context): Diary = {
+  def addDiary(title: String)(implicit ctx: Context): Either[Error, Diary] = {
     val user = currentUser
-    // QUESTION: 実はエラーを返したい。どうすれば綺麗にできるのか？Etherをつかう?Core的に処理したい。
+    // QUESTION: どうすれば綺麗にできるのか？
     repository.Diaries.findByUserAndTitle(user, title) match {
-      case None => repository.Diaries.create(user, title)
-      case _ => repository.Diaries.findByUserAndTitle(user, title).get
+      case None => Right(repository.Diaries.create(user, title))
+      case _ => Left(DiaryAlreadyExists)
+    }
+  }
+
+  def listDiary()(implicit ctx: Context): List[Diary] = {
+    val user = currentUser
+
+    repository.Diaries.listAll(user).toList
+  }
+
+  def addArticle(diaryTitle: String, title: String, body: String)(implicit ctx: Context): Either[Error, Article] = {
+    val user = currentUser
+
+    // QUESTION 以下ができる綺麗なコードは？
+    // すでに作成された同じダイアリーの同じタイトルの記事があるか確認
+    // ある場合、ArticleAlreadyExistsエラーを返す
+    // なかったら、記事を作成
+    repository.Diaries.findByUserAndTitle(user, diaryTitle) match {
+      case Some(diary) =>
+        repository.Articles.findByDiaryAndTitle(diary, title) match {
+          case None => Right(repository.Articles.create(diary, title, body))
+          case _ => Left(ArticleAlreadyExists)
+        }
+      case None => Left(DiaryNotFoundError)
+    }
+  }
+
+  def listArticle(diaryTitle: String)(implicit ctx: Context): Either[Error, List[Article]] = {
+    val user = currentUser
+
+    repository.Diaries.findByUserAndTitle(user, diaryTitle) match {
+      case Some(diary) =>
+        Right(repository.Articles.listAll(diary).toList)
+      case None => Left(DiaryNotFoundError)
     }
   }
 }
