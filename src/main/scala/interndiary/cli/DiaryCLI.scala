b4ca7f34501586b3282a67045aac416570ce58c5
voidsatisfaction <lourie@naver.com>
2018-05-10 18:19:34 +0900

    feat: add interface layer

--- src/main/scala/interndiary/cli/DiaryCLI.scala
+++ src/main/scala/interndiary/cli/DiaryCLI.scala
@@ -1,6 +1,8 @@
 package interndiary.cli
 
 import scala.sys.process
+import interndiary.model.{Article}
+import interndiary.service.{DiaryApp, Context}
 
 object DiaryCLI {
   def main(args: Array[String]): Unit = {
@@ -8,19 +10,127 @@ object DiaryCLI {
     sys.exit(exitStatus)
   }
 
+  private def createApp(userName: String): DiaryApp = new DiaryApp(userName)
+
   def run(args: Array[String]): Int = {
-    // TODO: implement
-    help()
+    try {
+      whiteSpace
+
+      Context.setup("db.default")
+      implicit val ctx = Context.createContext()
+      args.toList match {
+        case userName :: "addDiary" :: diaryTitle :: _ =>
+          // QUESTION: appを外に出したいが、綺麗にできる方法？
+          val app = createApp(userName)
+          addDiary(app, diaryTitle)
+        case userName:: "listDiary" :: _ =>
+          val app = createApp(userName)
+          listDiary(app)
+        case userName :: "addArticle" :: diaryTitle :: articleTitle :: articleBody :: _ =>
+          val app = createApp(userName)
+          addArticle(app, diaryTitle, articleTitle, articleBody)
+        case userName :: "listArticle" :: diaryTitle :: _ =>
+          val app = createApp(userName)
+          listArticle(app, diaryTitle)
+        case _ =>
+          help()
+      }
+    } finally Context.destroy
+  }
+
+  def addDiary(app: DiaryApp, title: String)(implicit ctx: Context): Int = {
+    app.addDiary(title) match {
+      case Right(diary) =>
+        println(s"diary ${diary.title} is added")
+        0
+      case Left(error) =>
+        process.stderr.println(error.toString())
+        1
+    }
+  }
+
+  def listDiary(app: DiaryApp)(implicit ctx: Context): Int = {
+    println(s"--- ${app.currentUser.name}'s Diary ---")
+    app.listDiary().zipWithIndex.foreach { case(diary, i:Int) =>
+      println(s"$i: ${diary.title}")
+    }
+    0
+  }
+
+  def addArticle(
+    app: DiaryApp,
+    diaryTitle: String,
+    articleTitle: String,
+    articleBody: String
+  )(implicit ctx: Context): Int = {
+    app.addArticle(diaryTitle, articleTitle, articleBody) match {
+      case Right(article) =>
+        printArticle(article, "verbose")
+        0
+      case Left(error) =>
+        process.stderr.println(error.toString())
+        1
+    }
+  }
+
+  def listArticle(
+    app: DiaryApp,
+    diaryTitle: String
+  )(implicit ctx: Context): Int = {
+    app.listArticle(diaryTitle) match {
+      case Right(articles) =>
+        articles.zipWithIndex.foreach { case(article, i) =>
+          println(s"-------$i-------")
+          printArticle(article, "verbose")
+        }
+        0
+      case Left(error) =>
+        process.stderr.println(error.toString())
+        1
+    }
   }
 
   def help(): Int = {
     process.stderr.println(
       """
         | usage:
-        |   run add url [comment]
-        |   run list
-        |   run delete url
+        |   run {username} addDiary {title}
+        |   run {username} listDiary
+        |   run {username} addArticle {diaryTitle} {title} {body}
+        |   run {username} listArticle {diaryTitle}
       """.stripMargin)
     1
+  }
+
+  private def whiteSpace(): Unit = {
+    println("\n\n")
+  }
+
+  private def printArticle(article: Article, option: String): Unit = {
+    option match {
+      case "verbose" =>
+        println("\n\n")
+        println("--------title-------")
+        println(article.title)
+        println("--------body-------")
+        println(article.body)
+        println("--------created-------")
+        println(s"@${article.created}")
+        println("--------last updated-------")
+        println(s"@${article.updated}")
+        println("\n\n")
+      case "title" =>
+        println("--------title-------")
+        println("\n\n")
+        println(article.title)
+        println("\n\n")
+      case _ =>
+        println("--------title-------")
+        println("\n\n")
+        println(article.title)
+        println("--------body-------")
+        println(article.body)
+        println("\n\n")
+    }
   }
 }
