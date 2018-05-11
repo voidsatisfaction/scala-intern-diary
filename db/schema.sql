9a78c3a3a1a07db99319fc8d97c6380a7f43662c
voidsatisfaction <lourie@naver.com>
2018-05-11 18:17:15 +0900

    fix: use datetime instead of timestamp

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `user_id` BIGINT UNSIGNED NOT NULL,
   `name` VARCHAR(32) NOT NULL,
-  `created` TIMESTAMP NOT NULL,
+  `created` DATETIME NOT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -12,13 +12,10 @@ CREATE TABLE `diary` (
   `diary_id` BIGINT UNSIGNED NOT NULL,
   `title` VARCHAR(150) NOT NULL,
   `user_id` BIGINT UNSIGNED NOT NULL,
-  `created` TIMESTAMP NOT NULL,
+  `created` DATETIME NOT NULL,
   PRIMARY KEY (`diary_id`),
   UNIQUE KEY (`title`, `user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- QUESTION: これないと、errorが出る。どうすれば良い？
-SET SQL_MODE='ALLOW_INVALID_DATES';
 
 DROP TABLE IF EXISTS `article`;
 CREATE TABLE `article` (
@@ -26,8 +23,8 @@ CREATE TABLE `article` (
   `title` VARCHAR(150) NOT NULL,
   `body` VARCHAR(5000) NOT NULL,
   `diary_id` BIGINT UNSIGNED NOT NULL,
-  `created` TIMESTAMP NOT NULL,
-  `updated` TIMESTAMP NOT NULL,
+  `created` DATETIME NOT NULL,
+  `updated` DATETIME NOT NULL,
   PRIMARY KEY (`article_id`),
   UNIQUE KEY (`title`, `diary_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
