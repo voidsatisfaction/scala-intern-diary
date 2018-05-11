80fd36f599aeedf3670b4dcfd9ce583dc23a7878
voidsatisfaction <lourie@naver.com>
2018-05-11 12:00:10 +0900

    fix: change error name

--- src/main/scala/interndiary/service/Error.scala
+++ src/main/scala/interndiary/service/Error.scala
@@ -3,12 +3,12 @@ package interndiary.service
 sealed trait Error {
   override def toString(): String = this match {
     case DiaryNotFoundError => "Can not find a target diary"
-    case DiaryAlreadyExists => "Can not add diary which already exists"
-    case ArticleNotFound => "Can not find a target article"
-    case ArticleAlreadyExists => "Can not add article which already exists"
+    case DiaryAlreadyExistsError => "Can not add diary which already exists"
+    case ArticleNotFoundError => "Can not find a target article"
+    case ArticleAlreadyExistsError => "Can not add article which already exists"
   }
 }
 final case object DiaryNotFoundError extends Error
-final case object DiaryAlreadyExists extends Error
-final case object ArticleNotFound extends Error
-final case object ArticleAlreadyExists extends Error
+final case object DiaryAlreadyExistsError extends Error
+final case object ArticleNotFoundError extends Error
+final case object ArticleAlreadyExistsError extends Error
