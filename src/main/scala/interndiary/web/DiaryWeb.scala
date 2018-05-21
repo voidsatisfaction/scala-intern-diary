017dedcb59616691cf3c5c5dc78a130eb2cd8e58
voidsatisfaction <lourie@naver.com>
2018-05-21 16:54:11 +0900

    feat: add login page and logic

--- src/main/scala/interndiary/web/DiaryWeb.scala
+++ src/main/scala/interndiary/web/DiaryWeb.scala
@@ -1,9 +1,42 @@
 package interndiary.web
 
+import interndiary.service.DiaryApp
 import org.scalatra._
 
-class DiaryWeb extends DiaryWebStack  {
+class DiaryWeb extends DiaryWebStack {
+  def createApp(): DiaryApp = {
+  }
+
+  def isLoggedIn(): Boolean = {
+    request.cookies.get("userName") match {
+      case Some(name) => true
+      case _ => false
+    }
+  }
+
+  // QUESTION: DiaryWebというclassの中で, getは関数？正体はなんだ？
   get("/") {
-    interndiary.html.index(msg = "Write your best diary, please!")
+    // QUSTION: 同じロジックが重複している(userLoggedIn)
+    implicit val userLoggedIn: Boolean = isLoggedIn
+    val app = createApp()
+    println(isLoggedIn)
+    interndiary.html.index("Write your best diary, please!")
+  }
+
+  get("/login") {
+    implicit val userLoggedIn: Boolean = isLoggedIn
+    interndiary.html.login()
+  }
+
+  post("/login") {
+    val userName: String = params("user_name")
+    response.setHeader(
+      "Set-Cookie", s"""userName=${userName}"""
+    )
+    redirect("/")
+  }
+
+  post("/logout") {
+
   }
 }
