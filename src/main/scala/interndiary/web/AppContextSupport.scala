528757d55a07afbf5f5f7529398407fff895402c
voidsatisfaction <lourie@naver.com>
2018-05-24 15:05:32 +0900

    feat: apply context

---
+++ src/main/scala/interndiary/web/AppContextSupport.scala
@@ -0,0 +1,27 @@
+// QUESTION: これはどうやって動いているのだ？
+// どうやってContextを注入しているのか？
+package interndiary.web
+
+import org.scalatra._
+import interndiary.service.Context
+
+object AppContextSupport {
+  val key = {
+    val n = getClass.getName
+    if (n.endsWith("$")) n.dropRight(1) else n
+  }
+}
+
+trait AppContextSupport { this: ScalatraBase =>
+
+  implicit def appContext = request.get(AppContextSupport.key).orNull.asInstanceOf[Context]
+
+  before() {
+    request(AppContextSupport.key) = Context.createContext
+  }
+
+  after() {
+    Context.releaseContext(appContext)
+  }
+
+}
